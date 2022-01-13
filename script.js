82aa7022fe06a6a7e6cf194d2ad1260f6cdb31ac
Viktoria Demchenko <Sovaviktorovna430@gmail.com>
2022-01-13 16:20:36 +0300

    Implement get result

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+const runButton = document.getElementById('button')
+const fromInput = document.getElementById('from_input')
+const toInput = document.getElementById('to_input')
+const resultField = document.getElementById('result_field')
 
 function getRandomBetween(min, max){
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-console.log(getRandomBetween(10, 42))
+function getResult(){
+  const result = getRandomBetween(fromInput.value, toInput.value)
+
+  resultField.textContent = result
+}
+
+runButton.onclick = getResult
